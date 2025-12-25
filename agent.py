3835a419abdfa12e24d260935a2049484f701bf6
Newt_Louis <l.ngoc.17395@gmail.com>
2025-12-25 23:46:45 +0700

    fix load evn for local develop

--- agent.py
+++ agent.py
@@ -6,15 +6,18 @@ from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
 
-
+system_prompt = SystemMessage(content="Bạn là chuyên gia tìm kiếm thông tin. Hãy dùng công cụ tavily để trả lời.")
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3-flash-preview",
-    temperature=0,
+    temperature=1.0,
     max_retries=2,
 )
 
@@ -59,23 +62,23 @@ workflow.add_edge("tools", "agent")
 app = workflow.compile()
 
 
-if __name__ == "__main__":
-    # Câu hỏi test
-    user_query = "Tìm kiếm thông tin về iPhone 16 mới nhất, nó đã ra mắt chưa?"
-
-    # System Prompt giúp Gemini định hình vai trò
-    system_prompt = SystemMessage(content="Bạn là chuyên gia tìm kiếm thông tin. Hãy dùng công cụ tavily để trả lời.")
-
-    inputs = {"messages": [system_prompt, HumanMessage(content=user_query)]}
-
-    # In ra luồng chạy
-    for output in app.stream(inputs):
-        for key, value in output.items():
-            print(f"Node '{key}': Đã chạy xong.")
-            # Uncomment dòng dưới nếu muốn xem nội dung chi tiết từng bước
-            # print(value)
-
-    print("\n--- KẾT QUẢ CUỐI CÙNG ---")
-    # Lấy tin nhắn cuối cùng từ AI
-    final_response = output['agent']['messages'][0].content
-    print(final_response)
+# if __name__ == "__main__":
+#     # Câu hỏi test
+#     user_query = "Tìm kiếm thông tin về iPhone 16 mới nhất, nó đã ra mắt chưa?"
+#
+#     # System Prompt giúp Gemini định hình vai trò
+#     system_prompt = SystemMessage(content="Bạn là chuyên gia tìm kiếm thông tin. Hãy dùng công cụ tavily để trả lời.")
+#
+#     inputs = {"messages": [system_prompt, HumanMessage(content=user_query)]}
+#
+#     # In ra luồng chạy
+#     for output in app.stream(inputs):
+#         for key, value in output.items():
+#             print(f"Node '{key}': Đã chạy xong.")
+#             # Uncomment dòng dưới nếu muốn xem nội dung chi tiết từng bước
+#             # print(value)
+#
+#     print("\n--- KẾT QUẢ CUỐI CÙNG ---")
+#     # Lấy tin nhắn cuối cùng từ AI
+#     final_response = output['agent']['messages'][0].content
+#     print(final_response)
